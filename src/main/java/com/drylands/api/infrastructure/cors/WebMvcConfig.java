415fc11320459a0aaefb0e446bc44bea74603fe7
Brendson Silva <brendson.net@gmail.com>
2023-09-04 17:46:01 -0300

    feat: criando projeto

---
+++ src/main/java/com/drylands/api/infrastructure/cors/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.drylands.api.infrastructure.cors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${url.web}")
+    private String origin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(origin)
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
---
+++ src/main/java/com/drylands/api/infrastructure/swagger/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.drylands.api.infrastructure.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(info = @Info(
+        contact = @Contact(name = "Dry Land's Modas Sistema"),
+        title = "Dry Land's Modas - API",
+        version = "0.0.1"),
+        security = @SecurityRequirement(name = "JWT Token"))
+@SecurityScheme(
+        name = "JWT Token",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+@Configuration
+public class SwaggerConfig {
+}
