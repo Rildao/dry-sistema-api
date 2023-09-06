98a959f729ba99102db8540628bf00f5de6eddee
MathLopes1 <iamatheuslopes@gmail.com>
2023-09-05 22:32:43 -0300

    feature(auth-service): iniciando fluxo de autenticação e criação de usuarios :rocket:

--- src/main/java/com/drylands/api/infrastructure/modelMapper/ModelMapperConfig.java
+++ src/main/java/com/drylands/api/infrastructure/modelMapper/ModelMapperConfig.java
@@ -1,2 +1,14 @@
-package com.drylands.api.infrastructure.modelMapper;public class ModelMapperConfig {
+package com.drylands.api.infrastructure.modelMapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
