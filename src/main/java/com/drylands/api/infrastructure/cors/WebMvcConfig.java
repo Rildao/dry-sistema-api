0816d1863dcc4e71fb4a2d1dc45b8f4dd94c5a2f
Brendson Silva <brendson.net@gmail.com>
2023-09-13 01:16:07 -0300

    feat: atualizando homolog

--- src/main/java/com/drylands/api/infrastructure/cors/WebMvcConfig.java
+++ src/main/java/com/drylands/api/infrastructure/cors/WebMvcConfig.java
@@ -18,10 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://" + originWeb,
-                        "https://" + originWeb,
-                        "http://" + originApi,
-                        "https://" + originApi
+                        "*"
                 )
                 .allowedMethods("*")
                 .allowedHeaders("*");
