7d70e51c2bf73eaf0bd61a5474750023be3671ef
MathLopes1 <iamatheuslopes@gmail.com>
2023-10-14 18:07:33 -0300

    feature(cliente): validação :rocket:

--- src/main/java/com/drylands/api/rest/dtos/cliente/ClienteVendasDTO.java
+++ src/main/java/com/drylands/api/rest/dtos/cliente/ClienteVendasDTO.java
@@ -1,6 +1,7 @@
 package com.drylands.api.rest.dtos.cliente;
 
 import com.drylands.api.rest.dtos.venda.VendaSimplificadoDTO;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.util.List;
 public class ClienteVendasDTO {
     private Long id;
 
+    @NotBlank(message = "O nome do cliente não pode estar em branco.")
     private String nome;
 
     private String cpf;
